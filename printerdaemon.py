841676a8dc8226d8b59ffecce514675fdecf4bc5
Pablo Iriarte <pablo.iriarte@unige.ch>
2025-11-27 12:35:08 +0100

    Update printerdaemon.py

--- printerdaemon.py
+++ printerdaemon.py
@@ -5,4 +5,16 @@
 # SLSP Forum 2025
 
 
+# Request to ALMA printouts
+
+# Parameters
+mylimit = 50
+apikey = 'headers = {'accept': 'application/json'}'
+mystatus = 'pending'
+headers = {'accept': 'application/json'}
+
+import requests
+myurl = 'https://api-eu.hosted.exlibrisgroup.com/almaws/v1/task-lists/printouts?status=' + mystatus + '&printer_id=' + printer_id + '&limit=' + mylimit + '&offset=0&apikey=' + apikey
+
+r = requests.get(myurl, headers=headers, timeout=15)
 
