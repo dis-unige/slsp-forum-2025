37f5bc38a7a97f69fcd55670a11e70513671446b
Pablo Iriarte <pablo.iriarte@unige.ch>
2025-11-27 12:39:23 +0100

    Update printerdaemon.py

--- printerdaemon.py
+++ printerdaemon.py
@@ -12,8 +12,10 @@ mylimit = 50
 apikey = "headers = {'accept': 'application/json'}"
 mystatus = 'pending'
 headers = "l8xx152e3d4717324728be090543e0808e0d"
+printer_id = 'I_HEPBE23'
 
 import requests
+
 myurl = 'https://api-eu.hosted.exlibrisgroup.com/almaws/v1/task-lists/printouts?status=' + mystatus + '&printer_id=' + printer_id + '&limit=' + mylimit + '&offset=0&apikey=' + apikey
 
 r = requests.get(myurl, headers=headers, timeout=15)
